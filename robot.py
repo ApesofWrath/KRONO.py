37c85e0b481bd76e5f3f588e60d31b3c04dbc943
zahtec <32808999+zahtec@users.noreply.github.com>
2024-06-15 16:54:44 -0700

    chore: basic robot class outline

--- robot.py
+++ robot.py
@@ -1,1 +1,29 @@
-# TODO: insert robot code here
+import commands2
+import wpilib
+import wpilib.drive
+
+
+class Robot(commands2.TimedCommandRobot):
+	def robotInit(self):
+		pass
+
+	def autonomousInit(self):
+		pass
+
+	def autonomousPeriodic(self):
+		pass
+
+	def teleopInit(self):
+		pass
+
+	def teleopPeriodic(self):
+		pass
+
+	def testInit(self):
+		pass
+
+	def testPeriodic(self):
+		pass
+
+	def robotPeriodic(self):
+		pass
